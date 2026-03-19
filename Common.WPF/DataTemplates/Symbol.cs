e82e57de56cf65dc8b1ea670eb91630bc709505f
ThinkerHua <thinkerhua@hotmail.com>
2026-03-19 10:47:56 +0800

    Common.WPF: 添加"WeldSymbols"资产及配套的"SymbolDataTemplate";

---
+++ Common.WPF/DataTemplates/Symbol.cs
@@ -0,0 +1,8 @@
+using System.Windows.Media;
+
+namespace Muggle.TsExtensions.Common.WPF.DataTemplates {
+    public class Symbol {
+        public string Name { get; set; }
+        public DrawingGroup Drawing  { get; set; }
+    }
+}
