b0b0242e0ab4d0bd5e96ae624e2fcace62db18f2
ThinkerHua <thinkerhua@hotmail.com>
2026-05-27 09:31:57 +0800

    MainWindow: 修复 'ProjectBeamOntoPlane' 未能正确移动梁对象的错误;

--- MainWindow/ViewModels/ProjectBeamOntoPlaneViewModel.cs
+++ MainWindow/ViewModels/ProjectBeamOntoPlaneViewModel.cs
@@ -1,17 +1,18 @@
 ﻿/*==============================================================================
  *  Muggle TsExtensions - extensions for Tekla Structures
  *
- *  Copyright © 2026 Huang YongXing.                 
+ *  Copyright © 2026 Huang YongXing.
  *
- *  This library is free software, licensed under the terms of the GNU 
- *  General Public License as published by the Free Software Foundation, 
- *  either version 3 of the License, or (at your option) any later version. 
- *  You should have received a copy of the GNU General Public License 
- *  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ *  This library is free software, licensed under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  *==============================================================================
  *  ProjectBeamOntoPlaneViewModel.cs: view model for the project beam onto plane operation.
  *  written by Huang YongXing - thinkerhua@hotmail.com
  *==============================================================================*/
+
 using System;
 using System.Collections;
 using System.Linq;
@@ -24,9 +25,7 @@ using Tekla.Structures.Model;
 using Tekla.Structures.Model.UI;
 
 namespace Muggle.TsExtensions.MainWindow.ViewModels {
-
     public partial class ProjectBeamOntoPlaneViewModel : ViewModelBase {
-
         public enum ProjectionDirectionEnum {
             Perpendicular,
             GlobalZ,
@@ -42,11 +41,9 @@ namespace Muggle.TsExtensions.MainWindow.ViewModels {
         private Tekla.Structures.Model.UI.ModelObjectSelector UiSelector { get; }
         private IMessageBoxService MessageBox { get; }
 
-        [ObservableProperty]
-        private ProjectionDirectionEnum _projectionDirection;
+        [ObservableProperty] private ProjectionDirectionEnum _projectionDirection;
 
-        [ObservableProperty]
-        private WayToGetPlaneEnum _wayToGetPlane;
+        [ObservableProperty] private WayToGetPlaneEnum _wayToGetPlane;
 
         public ProjectBeamOntoPlaneViewModel(IMessageBoxService messageBox) {
             ProjectionDirection = ProjectionDirectionEnum.GlobalZ;
@@ -60,65 +57,76 @@ namespace Muggle.TsExtensions.MainWindow.ViewModels {
 
         [RelayCommand]
         private void ProjectBeamOntoPlane() {
-            var selectedObjects = UiSelector.GetSelectedObjects();
-            if (selectedObjects.GetSize() == 0) {
+            var currentTp = Model.GetWorkPlaneHandler().GetCurrentTransformationPlane();
+            try {
+                Model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane());
+
+                var selectedObjects = UiSelector.GetSelectedObjects();
+                if (selectedObjects.GetSize() == 0) {
+                    try {
+                        selectedObjects = Picker.PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS);
+                    } catch (Exception e) when (e.Message == App.UserInterrupt) {
+                        return;
+                    } catch (Exception e) {
+                        MessageBox.ShowError(e.ToString());
+                        return;
+                    }
+                }
+
+                if (selectedObjects.GetSize() == 0) return;
+
+                Point[] points = null;
                 try {
-                    selectedObjects = Picker.PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS);
+                    switch (WayToGetPlane) {
+                    case WayToGetPlaneEnum.PickFace:
+                        var input = Picker.PickFace();
+
+                        foreach (InputItem item in input) {
+                            if (item.GetInputType() != InputItem.InputTypeEnum.INPUT_POLYGON) continue;
+                            points = [.. (item.GetData() as ArrayList)!.Cast<Point>()];
+                            break;
+                        }
+
+                        break;
+                    case WayToGetPlaneEnum.Pick3Points:
+                    default:
+                        points = new Point[3];
+
+                        points[0] = Picker.PickPoint();
+                        for (int i = 1; i < 3; i++) {
+                            points[i] = Picker.PickPoint(points[i - 1]);
+                        }
+
+                        break;
+                    }
                 } catch (Exception e) when (e.Message == App.UserInterrupt) {
                     return;
                 } catch (Exception e) {
                     MessageBox.ShowError(e.ToString());
                     return;
                 }
-            }
-            if (selectedObjects.GetSize() == 0) return;
 
-            Point[] points = null;
-            try {
-                switch (WayToGetPlane) {
-                case WayToGetPlaneEnum.PickFace:
-                    var input = Picker.PickFace();
+                var plane = new GeometricPlane(
+                    points![0], new Vector(points[1] - points[0]), new Vector(points[2] - points[0]));
 
-                    foreach (InputItem item in input) {
-                        if (item.GetInputType() != InputItem.InputTypeEnum.INPUT_POLYGON) continue;
-                        points = [.. (item.GetData() as ArrayList).Cast<Point>()];
-                        break;
-                    }
+                var direction = ProjectionDirection switch {
+                    ProjectionDirectionEnum.Perpendicular => plane.Normal,
+                    _ => new Vector(0, 0, 1000),
+                };
 
-                    break;
-                case WayToGetPlaneEnum.Pick3Points:
-                default:
-                    points = new Point[3];
-
-                    points[0] = Picker.PickPoint();
-                    for (int i = 1; i < 3; i++) {
-                        points[i] = Picker.PickPoint(points[i - 1]);
-                    }
-
-                    break;
+                foreach (var selectedObject in selectedObjects) {
+                    if (selectedObject is not Beam beam) continue;
+                    beam.StartPoint = IntersectionPoint(beam.StartPoint, direction, plane);
+                    beam.EndPoint = IntersectionPoint(beam.EndPoint, direction, plane);
+                    beam.Modify();
                 }
-            } catch (Exception e) when (e.Message == App.UserInterrupt) {
-                return;
-            } catch (Exception e) {
-                MessageBox.ShowError(e.ToString());
-                return;
+
+                Model.CommitChanges();
+            } catch {
+                // ignored
+            } finally {
+                Model.GetWorkPlaneHandler().SetCurrentTransformationPlane(currentTp);
             }
-
-            var plane = new GeometricPlane(points[0], new Vector(points[1] - points[0]), new Vector(points[2] - points[0]));
-
-            var direction = ProjectionDirection switch {
-                ProjectionDirectionEnum.Perpendicular => plane.Normal,
-                _ => new Vector(0, 0, 1000).TransformFrom(new TransformationPlane()),
-            };
-
-            foreach (var selectedObject in selectedObjects) {
-                if (selectedObject is not Beam beam) continue;
-                beam.StartPoint = IntersectionPoint(beam.StartPoint, direction, plane);
-                beam.EndPoint = IntersectionPoint(beam.EndPoint, direction, plane);
-                beam.Modify();
-            }
-
-            Model.CommitChanges();
         }
 
         private static Point IntersectionPoint(Point point, Vector vector, GeometricPlane plane) {
